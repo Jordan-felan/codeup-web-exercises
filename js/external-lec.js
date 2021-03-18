ebe1f09e5950b2f55439d11ac7257c3474722439
Jordan Felan <felan.jordan94@gmail.com>
2021-03-18 15:29:06 -0500

    completed th js with html lecture

---
+++ js/external-lec.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var message = "Howdy y'all!"
+
+var whatIsThisThen = message + " Hope y'all are having a mighty fine Thursday. "
+
+console.log(whatIsThisThen)
+
+//Back At 2:48
+
+/**
+*
+*Hello from the the other side
+*/
+//
+// var favoriteCheese = prompt("What is your favorite cheese?");
+//
+// console.log(favoriteCheese + " happens to be their favorite cheese. Whoop Whoop!");
+//
+// var typeOfPromt = typeof prompt("tell me something");
+//
+// var num = parseFloat(prompt("Gimme a number..."));
+//
+// console.log("User entered; " + num + ". That number plus 17 is: " + (num + 17) + ".");
+
+// var canIGoToToysAreRUs = confirm("Can i got to Toys R Us, pweaze?");
+//
+// console.log(canIGoToToysAreRUs);
+
+alert("You're looking at a website");
+
+
+
+
+
+
