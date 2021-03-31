067306b0e6c6aa6293b5eed89caadaa2c52ae879
Jordan Felan <felan.jordan94@gmail.com>
2021-03-31 09:07:24 -0500

    working on objects projects

---
+++ js/options-lec.js
@@ -0,0 +1,12 @@
+"use strict";
+var theTruck = {
+    make: "Toyota",
+    model: "Tacoma",
+    truck: true,
+    seats: 5,
+    color: "Voodoo Blue",
+    mileage: 10,
+    drive: function(){
+        console.log("vroom vroom"); //functions inside of an object are methods
+    }
+}
